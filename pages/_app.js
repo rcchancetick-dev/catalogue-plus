8411e90098c0f87be44fe1afd5e63bcf3f922fee
rcchancetick-dev <rcchancetick@gmail.com>
2026-09-05 03:57:14 +0300

    Importe le CSS responsive dans _app.js (en dernier pour priorite de surcharge)

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css';
 import '../styles/password-field.css';
+import '../styles/responsive-fixes.css';
 import { useEffect } from 'react';
 export default function App({ Component, pageProps }) {
   useEffect(() => { if ('serviceWorker' in navigator) window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => {})); }, []);
